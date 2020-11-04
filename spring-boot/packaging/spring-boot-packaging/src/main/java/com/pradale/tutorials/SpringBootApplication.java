bc90950b0252731abd2ce5697b57407cd1c5349e
pradeep <pradeepkudale@gmail.com>
2020-11-05 00:34:10 +0530

    Source code for spring boot packaging

---
+++ spring-boot/packaging/spring-boot-packaging/src/main/java/com/pradale/tutorials/SpringBootApplication.java
@@ -0,0 +1,7 @@
+package com.pradale.tutorials;
+
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello from SpringBootApplication...");
+    }
+}
